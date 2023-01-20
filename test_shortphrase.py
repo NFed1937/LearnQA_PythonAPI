20a3041587cd74c53ff256f8465889ac95bcc09d
Nadejda Fedorova <Nadejda.Fedorova@billing.ru>
2023-01-20 14:39:43 +0300

    Logger

--- test_shortphrase.py
+++
@@ -1,8 +0,0 @@
-class TestShortPhrase:
-    def test_short_phrase(self):
-        max_len = 15
-        phrase = input("Введите фразу короче 15 символов: ")
-        len_phr = len(phrase)
-        assert len_phr <= max_len, f"Длина введенной фразы '{phrase}' более {max_len} символов"
-
-# python -m pytest -s test_shortphrase.py -k "test_short_phrase"
